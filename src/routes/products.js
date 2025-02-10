a45274507a32aea67cd4d4ebe02291cf3b4213eb
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2025-02-10 11:31:25 +0200

    Updates

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,30 +1,15 @@
 const express = require('express');
-const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const authorize = require('../middleware/auth'); // Import authentication middleware
+
+const router = express.Router();
 const prisma = new PrismaClient();
 
-// Get all products
-router.get('/', async (req, res) => {
+// Get all products (Protected)
+router.get('/', authorize, async (req, res) => {
     try {
         const products = await prisma.products.findMany();
         res.json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// Change to SKU?
-// Get a single product by ID
-router.get('/:id', async (req, res) => {
-    try {
-        const product = await prisma.products.findUnique({
-            where: { id: parseInt(req.params.id) },
-        });
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).json({ message: 'Product not found' });
-        }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
