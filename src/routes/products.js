f8bad67f2cc6b2791c25d1bb1dd056cd1e05a3b3
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2025-02-10 10:53:56 +0200

    Updates

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const authorize = require('../middleware/auth'); // Import authentication middleware
-
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// Get all products (Protected)
-router.get('/', authorize, async (req, res) => {
+// Get all products
+router.get('/', async (req, res) => {
     try {
         const products = await prisma.products.findMany();
         res.json(products);
@@ -15,8 +13,9 @@ router.get('/', authorize, async (req, res) => {
     }
 });
 
-// Get a single product by ID (Protected)
-router.get('/:id', authorize, async (req, res) => {
+// Change to SKU?
+// Get a single product by ID
+router.get('/:id', async (req, res) => {
     try {
         const product = await prisma.products.findUnique({
             where: { id: parseInt(req.params.id) },
@@ -31,56 +30,6 @@ router.get('/:id', authorize, async (req, res) => {
     }
 });
 
-// Create a new product (Protected)
-router.post('/', authorize, async (req, res) => {
-    try {
-        const { sku, name, price, description, image } = req.body;
-        const product = await prisma.products.create({
-            data: {
-                sku,
-                name,
-                price,
-                description,
-                image,
-            },
-        });
-        res.status(201).json(product);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// Update a product by ID (Protected)
-router.put('/:id', authorize, async (req, res) => {
-    try {
-        const { sku, name, price, description, image } = req.body;
-        const product = await prisma.products.update({
-            where: { id: parseInt(req.params.id) },
-            data: {
-                sku,
-                name,
-                price,
-                description,
-                image,
-                updated_at: new Date(),
-            },
-        });
-        res.json(product);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// Delete a product by ID (Protected)
-router.delete('/:id', authorize, async (req, res) => {
-    try {
-        await prisma.products.delete({
-            where: { id: parseInt(req.params.id) },
-        });
-        res.status(204).send();
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
+// Put och update när vi har auth
 
 module.exports = router;
