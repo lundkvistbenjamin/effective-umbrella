2b4a1eca8bbd33d1a16d81f9c3f5d528954870c5
”lundkvistbenjamin” <lundkvistbenjamin@gmail.com>
2025-02-13 12:58:15 +0200

    Uploads

--- src/server.js
+++ src/server.js
@@ -5,31 +5,28 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-console.log(`Node.js ${process.version}`);
+// Use CORS to allow all origins
+app.use(cors());
 
-app.use(cors({
-    origin: [
-        "http://127.0.0.1:5500" // Update as necessary for frontend
-    ]
-}));
-
-app.get("/", (req, res) => {
-    console.log(req.myVar);
-    res.send("<h1>Hello!!!</h1>");
-});
-
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Serve uploaded images as static files from the persistent volume mounted at /app/uploads
+// Serve uploaded images as static files
 app.use('/uploads', express.static('/app/uploads'));
+
+// Example route
+app.get("/", (req, res) => {
+    res.send("<h1>Hello!!!</h1>");
+});
 
 // Import the products route
 const productsRouter = require('./routes/products');
 app.use('/products', productsRouter);
 
+// Start the server
 app.listen(PORT, () => {
     try {
-        console.log(`Running on http://localhost:${PORT}`);
+        console.log(`Server is running on http://localhost:${PORT}`);
     } catch (error) {
         console.error(error);
     }
