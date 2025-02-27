5f5116bfadc51054c23879ca0c2a8115cdb247dd
lundkvistbenjamin <lundkvistbenjamin@gmail.com>
2025-02-27 11:17:15 +0200

    push

---
+++ auth_new.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try {
+        const authHeader = req.headers["authorization"];
+        if (!authHeader) {
+            return res.status(401).json({ message: "Ingen token!" });
+        }
+
+        console.log(`Authorize jwt: ${authHeader}`);
+        const token = authHeader?.split(" ")[1];
+
+        // Verifiera JWT
+        const userData = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(`Token goodkänd för användare ${userData.sub} ${userData.name}`);
+
+        // Kolla om user är admin
+        if (userData.role !== "admin") {
+            return res.status(403).json({ message: "Du har inte tillgång. Endast admins!" });
+        }
+
+        // Annars returnar vi de
+        req.userData = userData;
+        next();
+
+    } catch (error) {
+        console.log(error.message);
+
+        res.status(401).send({
+            message: "Token inte godkänd!",
+            error: error.message
+        });
+    }
+};
